efb136762b7fa3bb15cd9b492d195e4fe5878a2b
Paulo <paulovictoralves.contato@gmail.com>
2023-05-05 12:52:33 -0300

    Adicionar 3 imagens, 3 variáveis

--- script/carrossel_receitas.js
+++ script/carrossel_receitas.js
@@ -1,1 +1,34 @@
-const listaImg = ["receitas/Pudim de leite condensado.png"]
+let slideAtual = 1;
+const slides = document.querySelectorAll(".imagem");
+
+function maisSlide(n) {
+    slideAtual +=n;
+    if (slideAtual > slides.length) {
+        slideAtual = 1;
+    } else if (slideAtual < 1) {
+        slideAtual = slides.length;
+    }
+    console.log(`slideAtual: ${slideAtual}`);
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    slides[slideAtual - 1].style.display = 'block';
+}
+
+function mostrarSlide(n) {
+    let i;
+    let slides = document.getElementsByClassName("imagem");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].classList.remove("aparecer");
+        slides[i].classList.add("desaparecer");
+    }
+    slides[slideIndex - 1].classList.remove("desaparecer");
+    slides[slideIndex - 1].classList.add("aparecer");
+}
