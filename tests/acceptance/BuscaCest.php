580d4a1bf360b797a24b3e38d839ecc2568c5bbe
paulovictor_alr <paulovictorodrigues03@gmail.com>
2023-05-31 11:13:13 -0300

    ADicionei a pesquisa

--- tests/acceptance/BuscaCest.php
+++ tests/acceptance/BuscaCest.php
@@ -20,3 +20,30 @@ class BuscaCest
 		$I->see('resultados encontdos');
     }
 }
+
+/*
+$I = new AcceptanceTester($scenario);
+$I->amOnPage('/html/index.html');
+$I->click('Cadastro');
+$I->seeCurrentURLEquals('/html/cadastro.html');
+$I->fillField('usuario', 'Ricardo');
+$I->fillField('senha', 'eletro');
+$I->fillField('cfsenha', 'eletro');
+$I->click('Fazer cadastro');
+$I->seeCurrentURLEquals('/html/login.html');
+*/
+
+/*
+$I = new AcceptanceTester($scenario);
+$I->amOnPage('/html/index.html');
+$I->click('Login');
+$I->seeCurrentURLEquals('/html/login.html');
+$I->fillField('usuario', 'Ricardo');
+$I->fillField('senha', 'eletro');
+$I->click('Fazer login');
+$I->seeCurrentURLEquals('/html/index.html');
+*/
+
+/*
+
+*/
