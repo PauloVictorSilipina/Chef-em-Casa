65164ce8a94da6dc7d0e4f305f4d21175ef0e74e
paulovictor_alr <paulovictorodrigues03@gmail.com>
2023-06-07 09:45:08 -0300

    Coloquei os testes

--- tests/acceptance/BuscaCest.php
+++ tests/acceptance/BuscaCest.php
@@ -35,6 +35,40 @@ class BuscaCest
       $I->seeCurrentURLEquals('/Website-Chef-em-Casa/login.html');
     }
 
+    public function buscarResultadosNaPaginaTest(AcceptanceTester $I)
+    {
+      $I->amOnPage('/Website-Chef-em-Casa/index.html');
+      $I->click('Contato');
+      $I->seeCurrentURLEquals('/contato.html');
+      $I->fillField('name', 'Ricardo Leite Rodrigues');
+      $I->fillField('mensagem', 'Olá, bom dia, gostaria de fazer um pedido para adicionarem mais receitas de doces!');
+      $I->click('Enviar');
+      $I->seeCurrentURLEquals('/Website-Chef-em-Casa/contato.html');
+    }
+
+    public function buscarResultadosNaPaginaTest(AcceptanceTester $I)
+    {
+      $I->amOnPage('/Website-Chef-em-Casa/index.html');
+      $I->click('bolo');
+      $I->seeCurrentURLEquals('/Website-Chef-em-Casa/receita.html');
+    }
+
+    public function buscarResultadosNaPaginaTest(AcceptanceTester $I)
+    {
+      $I->amOnPage('/Website-Chef-em-Casa/index.html');
+      $I->click('bolo');
+      $I->seeCurrentURLEquals('/Website-Chef-em-Casa/receita.html');
+    }
+
+    public function buscarResultadosNaPaginaTest(AcceptanceTester $I)
+    {
+      $I->amOnPage('/Website-Chef-em-Casa/index.html');
+      $I->click('Sobre');
+      $I->seeCurrentURLEquals('/Website-Chef-em-Casa/sobre.html');
+    }
+
+
+
 }
 
 /*
