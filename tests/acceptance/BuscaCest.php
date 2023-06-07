3b241d8d54a0524308b554ad6aed2b1421301673
paulovictor_alr <paulovictorodrigues03@gmail.com>
2023-06-07 09:35:48 -0300

    cadastro teste

--- tests/acceptance/BuscaCest.php
+++ tests/acceptance/BuscaCest.php
@@ -22,6 +22,19 @@ class BuscaCest
       $I->click('Fazer login');
       $I->seeCurrentURLEquals('/Website-Chef-em-Casa/index.html');
     }
+
+    public function buscarResultadosNaPaginaTest(AcceptanceTester $I)
+    {
+      $I->amOnPage('/Website-Chef-em-Casa/index.html');
+      $I->click('Cadastro');
+      $I->seeCurrentURLEquals('/cadastro.html');
+      $I->fillField('usuario', 'Ricardo');
+      $I->fillField('senha', '123456');
+      $I->fillField('cfsenha', '123456');
+      $I->click('Fazer cadastro');
+      $I->seeCurrentURLEquals('/Website-Chef-em-Casa/login.html');
+    }
+
 }
 
 /*
