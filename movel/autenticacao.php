ea6776fab29d5908e28dc398caf4f0b547521aa1
paulovictor_alr <84408596+PauloVictorSilipina@users.noreply.github.com>
2023-11-21 10:17:54 -0300

    Update autenticacao.php

--- movel/autenticacao.php
+++ movel/autenticacao.php
@@ -47,7 +47,7 @@ function autenticar($db_con) {
 		// o verifica junto a senha enviada ao servidor
 		if($consulta->rowCount() > 0){
 			$linha = $consulta->fetch(PDO::FETCH_ASSOC);
-			if(password_verify($senha, $linha['senha'])){ //Inicialmente estava token no lugar de senha 
+			if($senha == $linha['senha']){ //Inicialmente estava token no lugar de senha 
 				return true;
 			}
 		}
