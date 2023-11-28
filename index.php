3fff37b70c14e3d82a26f901e6c3e961996aa9ea
PauloVictorSilipina <paulovictorodrigues03@gmail.com>
2023-11-28 09:05:39 -0300

    Cadastro Receita quase feito

--- index.php
+++ index.php
@@ -4,11 +4,7 @@
     <meta charset='UTF-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-
     <?php include_once 'imports.html'?>
-
-    
-
     <link rel='stylesheet' href='css/css_home.css'>
     
     <title>Chef em Casa</title>
