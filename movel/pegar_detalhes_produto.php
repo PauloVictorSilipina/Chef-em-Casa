15d99e678f0984a344ead990611ecc5abcea97e3
paulovictor_alr <84408596+PauloVictorSilipina@users.noreply.github.com>
2023-12-07 12:48:23 -0300

    Add files via upload

--- movel/pegar_detalhes_produto.php
+++ movel/pegar_detalhes_produto.php
@@ -27,28 +27,15 @@ if(autenticar($db_con)) {
 		// Obtem do BD os detalhes do produto com id especificado na requisicao GET
 
 		$consultaInfoReceita = $db_con->prepare("
-		SELECT r.nome RecNome, m.url RecImg, r.porcao RecRend, r.temp_preparo RecTemp, d.dificuldade RecDif, u.img UsuImg, u.nome UsuNome, r.descricao RecDesc From RECEITA r
+		SELECT r.nome RecNome, m.url RecImg, r.porcao RecRend, r.temp_preparo RecTemp, r.dificuldade RecDif, u.img UsuImg, u.nome UsuNome, r.descricao RecDesc, mp.modo_preparo From RECEITA r
 		LEFT JOIN MIDIA m on m.FK_RECEITA_cod_rec = r.cod_rec 
 		LEFT JOIN USUARIO u on u.cod_perfil = r.FK_USUARIO_cod_perfil 
-		left join DIFICULDADE d on d.cod_dificuldade = r.FK_DIFICULDADE_cod_dificuldade 
+		left join MODO_PREPARO mp on mp.fk_RECEITA_cod_rec = r.cod_rec 
+		left join RECEITA_INGREDIENTE ri on ri.fk_RECEITA_cod_rec = r.cod_rec 
 		WHERE r.cod_rec = " . $id); // Referente ao criador, foto, dificuldade, etc (primeira coisa que aparece no web tbm)
 
-		$consultaIngrediente = $db_con->prepare("
-		SELECT ri.qtd, m.medida, i.nome FROM RECEITA r
-		left join RECEITA_INGREDIENTE ri on ri.fk_RECEITA_cod_rec = r.cod_rec 
-		LEFT JOIN INGREDIENTE i on i.cod_ingrediente  = ri.fk_INGREDIENTE_cod_ingrediente 
-		LEFT JOIN MEDIDA m on m.cod_medida = ri.fk_MEDIDA_cod_medida_ 
-		WHERE r.cod_rec = " . $id);
+		
 
-		$consultaModoPreparo = $db_con->prepare("
-		SELECT mp.modo_preparo FROM MODO_PREPARO mp 
-		left join RECEITA r on r.cod_rec = mp.FK_RECEITA_cod_rec 
-		WHERE r.cod_rec = " . $id);
-
-		$consultaComentarios = $db_con->prepare("
-		SELECT u.cod_perfil UsuCod, u.img, u.nome, c.COMENTARIO FROM COMENTA c  
-		LEFT JOIN USUARIO u on u.cod_perfil = c.fk_USUARIO_cod_perfil 
-		WHERE c.FK_RECEITA_cod_rec = " . $id);
 	 
 		if ($consulta->execute()) {
 			if ($consulta->rowCount() > 0) {
@@ -61,13 +48,32 @@ if(autenticar($db_con)) {
 				// a imagem do produto.
 				$linha = $consulta->fetch(PDO::FETCH_ASSOC);
 	 
-				$resposta["nome"] = $linha["nome"];
-				$resposta["preco"] = $linha["preco"];
-				$resposta["descricao"] = $linha["descricao"];
-				$resposta["img"] = $linha["img"];
-				$resposta["criado_em"] = $linha["criado_em"];
-				$resposta["criado_por"] = $linha["usuarios_login"];
+				$resposta["nome"] = $linha["RecNome"];
+				$resposta["descricao"] = $linha["RecDesc"];
+				$resposta["img"] = $linha["RecImg"];
+				$resposta["criado_por"] = $linha["UsuNome"];
+				$resposta["dificuldade"] = $linha["RecDif"];
+				$resposta["rendimento"] = $linha["RecRend"];
+				$resposta["tempo"] = $linha["RecTemp"];
+				$list = [];
+				$prepList = explode(";",$linha["modo_preparo"]);
+				for(i = 0;i<sizeof(prepList);i++){
+					array_push($list,$prepList[i]);
+				}
+				$resposta["preparo"] = $list;
 				
+				$consultaIngrediente = $db_con->prepare("
+				SELECT ri.qtd, ri.medida, ri.ingrediente FROM RECEITA_INGREDIENTE ri
+				WHERE ri.fk_RECEITA_cod_rec = " . $id);
+				
+				$consulta->execute();
+				$linha = $consulta->fetch(PDO::FETCH_ASSOC);
+				list = [];
+				for(i = 0;i<sizeof($linha);i++){
+					$string = $linha[i]["qtd"] + " " + $linha[i]["medida"] + " de " + $linha[i]["ingrediente"];
+					array_push($list,$string);
+				}
+				$resposta["ingredientes"] = $list;
 				// Caso o produto exista no BD, o cliente 
 				// recebe a chave "sucesso" com valor 1.
 				$resposta["sucesso"] = 1;
